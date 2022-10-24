5beed2d10defc481ef539cef16fd27fbe66c5e62
VAZ Frederico-EXT <fredvaz8@gmail.com>
2022-10-24 17:00:00 +0000

    config

---
+++ configs/VS_Code_Template_Project/main.cpp
@@ -0,0 +1,30 @@
+/*
+
+The C++20 Masterclass: From Fundamentals to Advanced
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void test1()
+{
+    /*
+    Notes:
+    */
+}
+
+int main()
+{
+    /*
+    General Notes:
+        Test1
+    */
+
+    cout << "\nTest 1:" << endl;
+    test1();
+
+    cout << endl;
+    return 0;
+}
