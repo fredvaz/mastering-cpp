f19b063a0e8c653b352532858505dc3e809b2a0e
VAZ Frederico-EXT <fredvaz8@gmail.com>
2023-01-02 23:15:00 +0000

    character manipulation

--- source/character_manipulation_strings/copied_strings.cpp
+++ source/character_manipulation_strings/copied_strings.cpp
@@ -9,12 +9,13 @@ General Notes:
    
 */
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 //
 
-#define TEST_CASE_1
+// #define TEST_CASE_1
 #ifdef TEST_CASE_1
 /*
     Notes:  #define TEST_CASE_1
@@ -28,7 +29,14 @@ int main()
 {
     cout << "------------------------------ Test Case 1 -------------------------------------" << endl;
 
-    //
+    std::string message{"Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!vvHello world!Hello world!Hello world!Hello world!Hello world!"};
+    std::string &message_copy{message};
+
+    char *p1 = message.data();
+    char *p2 = message_copy.data();
+
+    std::cout << "&message[0] : " << (void *)p1 << std::endl;
+    std::cout << "&message_copy[0] : " << (void *)p2 << std::endl;
 
     cout << "--------------------------------------------------------------------------------" << endl;
     return 0;
@@ -58,7 +66,7 @@ int main()
     return 0;
 }
 
-#endif // TEST_CASE_2 END
+#endif // TEST_CASE_N END
 
 //
 
--- source/character_manipulation_strings/escape_sequences.cpp
+++ source/character_manipulation_strings/escape_sequences.cpp
@@ -9,12 +9,13 @@ General Notes:
    
 */
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 //
 
-#define TEST_CASE_1
+// #define TEST_CASE_1
 #ifdef TEST_CASE_1
 /*
     Notes:  #define TEST_CASE_1
@@ -28,7 +29,29 @@ int main()
 {
     cout << "------------------------------ Test Case 1 -------------------------------------" << endl;
 
-    //
+    /*
+    for(size_t i{0} ; i  < 1000 ; ++i){
+        std::cout << "Hello\n";
+    }
+    */
+
+    /*
+    std::cout << "He said \"Get out of here immediately!\"" << std::endl;
+    */
+
+    /*
+    std::string todo_list{ "\tClean the house\n\tWalk the dog\n\tDo laundry\n\tPick groceries" };
+    std::string windows_path{ "D:\\sandbox\\testProject\\hello.txt" };
+    std::string linux_path{ "/home/username/files/hello.txt" };
+    std::string hint{ " \"\\\\\" escapes a backslash character like \\." };
+    std::cout << "todo_list : " << std::endl;
+    std::cout << todo_list << std::endl;
+    std::cout << "windows_path : " << windows_path << std::endl;
+    std::cout << "linux_path : " << linux_path << std::endl;
+    std::cout << "hint : " << hint << std::endl;
+    */
+
+    std::cout << "\a" << std::endl;
 
     cout << "--------------------------------------------------------------------------------" << endl;
     return 0;
@@ -58,7 +81,7 @@ int main()
     return 0;
 }
 
-#endif // TEST_CASE_2 END
+#endif // TEST_CASE_N END
 
 //
 
