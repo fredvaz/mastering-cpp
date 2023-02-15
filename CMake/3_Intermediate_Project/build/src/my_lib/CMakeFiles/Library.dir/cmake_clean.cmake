ca292e6dedf62641cff9b1091a7a671725f18604
fredvaz <fredvaz8@gmail.com>
2023-02-15 22:08:19 +0000

    CMake project structure

---
+++ CMake/3_Intermediate_Project/build/src/my_lib/CMakeFiles/Library.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Library.dir/my_lib.cc.o"
+  "libLibrary.a"
+  "libLibrary.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Library.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
