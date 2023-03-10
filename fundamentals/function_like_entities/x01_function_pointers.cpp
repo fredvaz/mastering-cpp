45e661060add34010e509515b1b0375b40f02f13
fredvaz <fredvaz8@gmail.com>
2023-03-10 23:24:12 +0000

    first steps update

--- fundamentals/function_like_entities/x01_function_pointers.cpp
+++ fundamentals/function_like_entities/x01_function_pointers.cpp
@@ -3,6 +3,12 @@
 The C++20 Masterclass Section 46: Function Like Entities
 
 General Notes:
+        * Things that can work as functions in C++
+        * Taking input
+        * Doing something in the function body
+        * Returning values
+        Callback functions are heavily used in event based programming like Graphcal User Interfaces
+        but that's out of scope for this course
 
 
    [REF]:
@@ -18,6 +24,10 @@ General Notes:
     !Note: #define EXAMPLE_1
 
             * EXAMPLE: Function Pointers
+            * A function is just a block of code that lives somewhere in the memory map of our C++ program. 
+            * We can grab the address of the function and store it in a function pointer.
+            
+
 */
 
 double add(double a, double b)
@@ -28,6 +38,8 @@ double add(double a, double b)
 int main()
 {
     std::cout << "------------------------------ EXAMPLE_1 -------------------------------------" << std::endl;
+
+    //* Different ways of setting a function Pointer
 
     // double (*f_ptr) (double, double)  = &add;
     // double(*f_ptr) (double,double) = add;
@@ -41,10 +53,10 @@ int main()
     // auto *f_ptr = &add;
     // auto *f_ptr = add;
 
-    // Things gone wroong
+    //! Things gone wroong
     double (*f_ptr)(double, double) = nullptr;
 
-    std::cout << "add(10,30) : " << f_ptr(10, 30) << std::endl;
+    std::cout << "add(10,30): " << f_ptr(10, 30) << std::endl;
 
     std::cout << "------------------------------------------------------------------------------" << std::endl;
     return 0;
